90bbf00c9382b03341ec24d9809cc4963caeda16
elisecarman <77816390+elisecarman@users.noreply.github.com>
2021-06-05 17:51:54 -0400

    5-51

--- pkg/miner/mine.go
+++ pkg/miner/mine.go
@@ -7,9 +7,7 @@ import (
 	"BrunoCoin/pkg/utils"
 	"context"
 	"encoding/hex"
-	"errors"
 	"fmt"
-	"log"
 	"math"
 )
 
@@ -149,10 +147,10 @@ func (m *Miner) GenCBTx(txs []*tx.Transaction) *tx.Transaction {
 
 	} else {
 		fmt.Printf("ERROR {tp.GenCBTx}: " + "nil transactions:  were given to the function")
-	//trxFake := proto.NewTx(m.Conf.Ver, []*proto.TransactionInput{}, []*proto.TransactionOutput{}, m.Conf.DefLckTm)
-	//return tx.Deserialize(trxFake)
-	log.Fatal(errors.New("nil transactions"))
-	return nil //this is where program crashes
+	trxFake := proto.NewTx(m.Conf.Ver, []*proto.TransactionInput{}, []*proto.TransactionOutput{}, m.Conf.DefLckTm)
+	return tx.Deserialize(trxFake)
+	//log.Fatal(errors.New("nil transactions"))
+	//return nil //this is where program crashes
 	}
 }
 
